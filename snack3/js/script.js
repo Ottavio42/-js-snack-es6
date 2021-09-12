e2e994c3cb6d543ac3248a7ad2043ea1207bd914
Ottavio42 <ottaviog88@gmail.com>
2021-09-12 20:38:09 +0200

    fine-esercizio3

---
+++ snack3/js/script.js
@@ -0,0 +1,22 @@
+const CutArray = (array, a, b) => {
+    let nuovoArray = array.filter((element,index)=>(index>=a && index<b));
+
+    // const nuovoArray=[];
+    // array.forEach((element,index) => {
+    //     if(index>=a && index<b){
+    //         nuovoArray.push(element);
+    //     }
+    // });
+    return nuovoArray;
+    
+};
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+let min = 3;
+let max = 9;
+
+let xArray = CutArray(num, min, max);
+
+console.log(xArray)
+
+
